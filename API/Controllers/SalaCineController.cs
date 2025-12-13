b4979dafe445476e9cd4832529bdb838595dd271
Rae1206 <alex.alvaradot@ug.edu.ec>
2025-12-12 23:45:54 -0500

    1

--- API/Controllers/SalaCineController.cs
+++ API/Controllers/SalaCineController.cs
@@ -50,13 +50,6 @@ public class SalaCineController : ControllerBase
         return Ok(response);
     }
 
-    [HttpPut("ActivarSalaCine/{id}")]
-    public async Task<IActionResult> ActivarSalaCine(int id)
-    {
-        var response = await _salaCineApplication.ActivateAsync(id);
-        return Ok(response);
-    }
-
     [HttpGet("DisponibilidadPorNombre/{nombre}")]
     public async Task<IActionResult> GetDisponibilidadByNombre(string nombre)
     {
